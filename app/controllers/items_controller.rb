2526abe115f552c3ed7bf730b3c1b6a94422ddb7
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2023-10-07 18:34:11 +0900

    controller編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_item,           only: [:show, :edit, :update]
+  before_action :move_to_index,      only: [:edit]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -41,5 +42,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :delivery_cost_id,:shipping_area_id, :shipping_lead_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
