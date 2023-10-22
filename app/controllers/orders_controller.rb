bef6b3fde4abac51d88f0fe2e7cc3500ecf3955f
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2023-10-22 20:07:33 +0900

    order_controller修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -22,7 +22,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery_information).permit(:postal_code, :shipping_area_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_delivery_information).permit(:postal_code, :shipping_area_id, :city, :street_address, :building_name, :phone_number ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item
