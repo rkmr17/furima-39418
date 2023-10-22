c3ec75e590be4b5648a888cb92debdc26f7a89be
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2023-10-22 20:08:07 +0900

    バリデーションリファクタリング

--- app/models/order_delivery_information.rb
+++ app/models/order_delivery_information.rb
@@ -2,14 +2,16 @@ class OrderDeliveryInformation
   include ActiveModel::Model
   attr_accessor :postal_code, :shipping_area_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id, :token
 
-  validates :postal_code,      presence:    true, format:       { with: /\A[0-9]{3}-[0-9]{4}\z/}
-  validates :shipping_area_id, presence:    true, numericality: { other_than: 1 }
-  validates :city,             presence:    true
-  validates :street_address,   presence:    true
-  validates :phone_number,     presence:    true, format:       { with: /\A\d{10,11}\z/ }
-  validates :user_id,          presence: true
-  validates :item_id,          presence: true
-  validates :token,            presence: true
+  with_options presence: true do
+    validates :postal_code,      format:       { with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :shipping_area_id,  numericality: { other_than: 1 }
+    validates :city
+    validates :street_address
+    validates :phone_number,      format:       { with: /\A\d{10,11}\z/ }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
